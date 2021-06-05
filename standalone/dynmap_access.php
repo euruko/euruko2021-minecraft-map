06169ed0aa39125e1b58a65b3744fcc2f651b6d0
Matias Korhonen <me@matiaskorhonen.fi>
2021-06-05 13:02:14 +0300

    Initial commit

---
+++ standalone/dynmap_access.php
@@ -0,0 +1,5 @@
+<?php
+$tilespath = '/home/minecraft/multicraft/servers/server738090/plugins/dynmap/web/tiles/';
+$markerspath = '/home/minecraft/multicraft/servers/server738090/plugins/dynmap/web/tiles/';
+$webpath = '/home/minecraft/multicraft/servers/server738090/plugins/dynmap/web/';
+?>
